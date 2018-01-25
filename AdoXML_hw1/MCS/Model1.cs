e951ebf2538797030fcc887ff428d68cc7581df1
СамигулинТ <@403_8_>
2018-01-25 18:02:46 +0600

    5588

---
+++ AdoXML_hw1/MCS/Model1.cs
@@ -0,0 +1,24 @@
+namespace AdoXML_hw1.MCS
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class Model1 : DbContext
+    {
+        public Model1()
+            : base("name=Model1")
+        {
+        }
+
+        public virtual DbSet<Area> Area { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Area>()
+                .Property(e => e.IP)
+                .IsUnicode(false);
+        }
+    }
+}
--- AdoXML_hw1/Program.cs
+++ AdoXML_hw1/Program.cs
@@ -3,13 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AdoXML_hw1.MCS;
+using System.IO;
+using System.Xml.Linq;
 
 namespace AdoXML_hw1
 {
     class Program
     {
+       public static Model1 db = new Model1();
         static void Main(string[] args)
         {
+            Task1();
         }
+
+        static void Task1()
+        {
+
+            var query = db.Area.Where(w => w.PavilionId == 1);
+
+            foreach(Area item in query)
+            {
+                XDocument xDoc = new XDocument(
+                    new XElement("Area",
+                new XElement("AreaId", item.AreaId),
+                new XElement("Name", item.Name),
+                new XElement("ParentId", item.ParentId)));
+                xDoc.Save(item.AreaId + ".xml");
+            }
+
+        }
+
+        static void Task2()
+        {
+            foreach(var item in db.Area)
+            {
+                DirectoryInfo dir = new DirectoryInfo(@"area\" + item.Name + "(" + item.AreaId + ")");
+                dir.Create();
+                XDocument xDoc = new XDocument(
+                   new XElement("Area",
+               new XElement("AreaId", item.AreaId),
+               new XElement("Name", item.Name),
+               new XElement("ParentId", item.ParentId)));
+                xDoc.Save(dir + ".xml");
+            }
+
+
+        }
+
     }
 }
